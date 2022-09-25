a468c553e5d0f94093f1335d633adc2d6011bf6a
Damian Grecki <greku112@gmail.com>
2022-09-25 11:37:14 +0200

    Change package name

--- src/main/java/org/mockserver/SecurityConfig.java
+++ src/main/java/org/mockserver/SecurityConfig.java
@@ -1,4 +1,4 @@
-package test.mockserver;
+package org.mockserver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/org/mockserver/controller/UnauthorizedMockController.java
+++ src/main/java/org/mockserver/controller/UnauthorizedMockController.java
@@ -1,4 +1,4 @@
-package test.mockserver.controller;
+package org.mockserver.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
